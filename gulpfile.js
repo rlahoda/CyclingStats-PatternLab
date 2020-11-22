05d6e2d657d5887c48231c33d8ca474487de42fe
Rob Lahoda <rob@roblahoda.com>
2020-11-21 23:17:40 -0500

    Reorganizing SCSS to make more sense

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ const sourcemaps = require("gulp-sourcemaps");
 
 function scss() {
   return (
-    src("./source/css/**/*.scss")
+    src("./source/_patterns/**/*.scss")
       .pipe(sass())
       .on("error", sass.logError)
       // Use postcss with autoprefixer and compress the compiled file using cssnano
@@ -19,10 +19,26 @@ function scss() {
       .pipe(browserSync.stream())
   );
 }
+// function scss() {
+//   return (
+//     src("./source/css/**/*.scss")
+//       .pipe(sass())
+//       .on("error", sass.logError)
+//       // Use postcss with autoprefixer and compress the compiled file using cssnano
+//       .pipe(postcss([autoprefixer(), cssnano()]))
+//       // Now add/write the sourcemaps
+//       .pipe(sourcemaps.write())
+//       .pipe(dest("source/css"))
+//       .pipe(browserSync.stream())
+//   );
+// }
 
 exports.css = scss;
 exports.compile = function() {
-  watch("source/css/**/*.scss", { ignoreInitial: false }, scss);
+  watch("source/_patterns/**/*.scss", { ignoreInitial: false }, scss);
 };
+// exports.compile = function() {
+//   watch("source/css/**/*.scss", { ignoreInitial: false }, scss);
+// };
 // const watch = () => watch(paths.scripts.src, gulp.series(scripts, reload));
 exports.default = parallel(scss);
